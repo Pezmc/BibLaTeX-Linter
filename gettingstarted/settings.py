888d0fc6a440fcd94703738dc82a4215fd3893b0
Pez Cuckow <email@pezcuckow.com>
2021-02-04 12:24:57 +0100

    🚚 Rename hello folder

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead).'
+SECRET_KEY = 'L1e2dIe8ddHLizQ4YEavcsmpAr3dhIpY.'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'linting'
 ]
 
 MIDDLEWARE = [
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,14 +4,13 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import linting.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^validate', hello.views.validate, name='validate'),
-    path('admin/', admin.site.urls),
+    url(r'^$', linting.views.index, name='index'),
+    url(r'^validate', linting.views.validate, name='validate'),
 ]
